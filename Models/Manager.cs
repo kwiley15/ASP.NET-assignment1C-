97c378036b3425d266d4f076a6d6e98d8b0acf94
kadenrb <kadenrb@gmail.com>
2025-02-04 16:46:18 -0700

    Create Manager.cs

---
+++ Models/Manager.cs
@@ -0,0 +1,17 @@
+﻿namespace assignment1C_.Models
+{
+	public class Manager
+	{
+		[Required(ErrorMessage = "First name is required")]
+		public string firstName { get; set;}
+		[Required(ErrorMessage = "Last name is required")]
+		public string lastName { get; set;}
+		[Required(ErrorMessage = "Phone number is required")]
+		public string phoneNumber { get; set;}
+		[Required(ErrorMessage="Email is required")]
+		public string email { get; set;}
+		[Required(ErrorMessage = "CategoryId is required"),Range(1,99999)]
+		public int categoryId { get; set;}
+		public string? organization {  get; set;}
+	}
+}
