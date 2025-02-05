1dcc690d9576d56f8461fd2d3fa8dfa8bd6bec7b
kadenrb <kadenrb@gmail.com>
2025-02-05 15:37:54 -0700

    Finished up the basic model templates

--- Models/Manager.cs
+++ Models/Manager.cs
@@ -1,17 +1,18 @@
-﻿namespace assignment1C_.Models
+﻿using System.ComponentModel.DataAnnotations;
+namespace assignment1C_.Models
 {
 	public class Manager
 	{
 		[Required(ErrorMessage = "First name is required")]
-		public string firstName { get; set;}
+		public string FirstName { get; set;}
 		[Required(ErrorMessage = "Last name is required")]
-		public string lastName { get; set;}
+		public string LastName { get; set;}
 		[Required(ErrorMessage = "Phone number is required")]
-		public string phoneNumber { get; set;}
+		public string PhoneNumber { get; set;}
 		[Required(ErrorMessage="Email is required")]
-		public string email { get; set;}
+		public string Email { get; set;}
 		[Required(ErrorMessage = "CategoryId is required"),Range(1,99999)]
-		public int categoryId { get; set;}
-		public string? organization {  get; set;}
+		public int CategoryId { get; set;}
+		public string? Organization {  get; set;}
 	}
 }
---
+++ Models/ManagerContext.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+namespace assignment1C_.Models
+{
+	public class ManagerContext : DbContext
+	{
+		public ManagerContext(DbContextOptions<ManagerContext> options)
+			: base(options) 
+		{ }
+		public DbSet<Manager> Managers { get; set; }
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+			modelBuilder.Entity<Manager>().HasData(
+				new Manager
+				{
+					FirstName = "Juan",
+					LastName = "Guerro",
+					PhoneNumber = "1234567890",
+					Email = "juangg@gmail.com",
+					CategoryId = 1,
+					Organization = "RDP"
+				},
+				new Manager
+				{
+					FirstName = "Elara",
+					LastName = "Starseeker",
+					PhoneNumber = "1234567890",
+					Email = "elara.starseeker@stellarnet.com ",
+					CategoryId = 1,
+					Organization = "Stellar Explorations"
+				},
+				new Manager
+				{
+					FirstName = "Cassia",
+					LastName = "Moonshade",
+					PhoneNumber = "7788990011",
+					Email = "cassia.moonshade@lunarchive.us ",
+					CategoryId = 5,
+					Organization = "Celestial Archives"
+				},
+				new Manager
+				{
+					FirstName = "Thorne",
+					LastName = "Blackthorn",
+					PhoneNumber = "9988776655",
+					Email = "thorne.blackthorn@shadowcraft.io ",
+					CategoryId = 4,
+					Organization = "Shadowbound Guild"
+				},
+				new Manager
+				{
+					FirstName = "Mira",
+					LastName = "Luminaris",
+					PhoneNumber = "1122334455",
+					Email = "mira.luminaris@lightweave.org ",
+					CategoryId = 3,
+					Organization = "Weavers of Dawn"
+				}
+				);
+		}
+
+	}
+}
